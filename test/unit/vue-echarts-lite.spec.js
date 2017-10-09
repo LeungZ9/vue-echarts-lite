e9c79deaeec7073a45772b00da139f27a534c783
LeungZ9 <31878600+LeungZ9@users.noreply.github.com>
2017-10-09 18:26:56 +0800

    Update Unit test

--- test/unit/vue-echarts-lite.spec.js
+++ test/unit/vue-echarts-lite.spec.js
@@ -1,14 +1,31 @@
 import Vue from 'vue';
 import VueEchartsLite from '../../src/vue-echarts-lite.vue';
 
+const miniConf = {
+    xAxis: {
+        name: 'x',
+        data: []
+    },
+    yAxis: {
+        name: 'y',
+    },
+    series: [
+        {
+            type: 'line',
+            data: []
+        }
+    ]
+};
+
 describe('vue-echarts-lite.vue', () => {
     it('should render correct contents', () => {
         const Constructor = Vue.extend(VueEchartsLite);
         const vm = new Constructor({
             propsData: {
-                config: {},
+                config: miniConf
             }
-        });
-        expect(vm.$el);
+        }).$mount();
+        expect(vm.$el.getAttribute('_echarts_instance_')).to.match(/^ec_\d{13}$/);
+        expect(vm.$el.querySelector('canvas')).to.not.be.null;
     });
 });
